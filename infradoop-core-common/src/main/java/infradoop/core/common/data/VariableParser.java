fe08b3bf57928fe2bbcad800562ab278b56ca835
Jared González <danjaredg@gmail.com>
2018-03-22 10:25:36 -0600

    Uso de constantes para delimitadores de expresión

--- infradoop-core-common/src/main/java/infradoop/core/common/data/VariableParser.java
+++ infradoop-core-common/src/main/java/infradoop/core/common/data/VariableParser.java
@@ -3,15 +3,18 @@ package infradoop.core.common.data;
 import org.apache.commons.lang.StringUtils;
 
 public class VariableParser {
+	private static final String START_EXPRESSION = "$"+"{";
+	private static final String END_EXPRESSION = "}";
+	
 	public static String parse(String data, VariableResolver resolver) {
 		if (data == null)
 			return null;
 		StringBuilder sb = new StringBuilder(data);
 		int offset = 0;
-		while ((offset = sb.indexOf("${", offset)) >= 0) {
-			int endOffset = sb.indexOf("}", offset);
+		while ((offset = sb.indexOf(START_EXPRESSION, offset)) >= 0) {
+			int endOffset = sb.indexOf(END_EXPRESSION, offset);
 			if (endOffset >= 0) {
-				String var = sb.substring(offset+2, endOffset);
+				String var = sb.substring(offset+START_EXPRESSION.length(), endOffset);
 				String params[];
 				if (var.contains(",")) {
 					String vararr[] = StringUtils.split(var, ",");
@@ -23,7 +26,7 @@ public class VariableParser {
 				}
 				sb.replace(offset, endOffset+1, resolver.resolve(var, params));
 			} else {
-				offset+=2;
+				offset+=START_EXPRESSION.length();
 			}
 		}
 		return sb.toString();
