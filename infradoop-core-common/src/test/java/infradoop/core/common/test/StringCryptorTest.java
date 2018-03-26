6b0eeb722e4cf78be003438ea9b77a5a66bcb6c8
Jared González <danjaredg@gmail.com>
2018-03-26 11:58:41 -0600

    Se incluye validacion de desencripción

--- infradoop-core-common/src/test/java/infradoop/core/common/test/StringCryptorTest.java
+++ infradoop-core-common/src/test/java/infradoop/core/common/test/StringCryptorTest.java
@@ -1,14 +1,24 @@
 package infradoop.core.common.test;
 
 import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import infradoop.core.common.StringCryptor;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class StringCryptorTest {
 	@Test
-	public void test() {
+	public void test_01_encrypt() {
 		String enc = StringCryptor.encrypt("Jared González");
 		Assert.assertEquals(StringCryptor.decrypt(enc), "Jared González");
+	}
+	
+	@Test
+	public void test_01_decript() {
+		Assume.assumeNotNull(System.getProperty("stringEncrypted"));
+		System.out.println(StringCryptor.decrypt(System.getProperty("stringEncrypted")));
 	}
 }
